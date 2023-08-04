1732b0f7b9f589d7c39d9fe95b750dc26e1a898e
tomasz-lipka <36009966+tomasz-lipka@users.noreply.github.com>
2023-08-04 15:53:55 +0000

    Refactor test case

--- project/app.py
+++ project/app.py
@@ -10,19 +10,22 @@ from src.repository.sql_alchemy_repository import SqlAlchemyRepository
 from src.model.user import User
 
 
-app = Flask(__name__)
-app.register_blueprint(user_controller)
-app.register_blueprint(space_controller)
-app.register_blueprint(assignment_controller)
-app.register_blueprint(share_controller)
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(user_controller)
+    app.register_blueprint(space_controller)
+    app.register_blueprint(assignment_controller)
+    app.register_blueprint(share_controller)
+    # Generates a 32-byte (64-character) random hex string
+    app.config["SECRET_KEY"] = secrets.token_hex(32)
+
+    return app
 
 
-# Generates a 32-byte (64-character) random hex string
-app.config["SECRET_KEY"] = secrets.token_hex(32)
+app = create_app()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-
 
 @login_manager.user_loader
 def load_user(user_id):
