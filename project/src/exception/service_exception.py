524ea266de8007be67e44da7c931cdc8bb4012be
tomasz-lipka <36009966+tomasz-lipka@users.noreply.github.com>
2023-08-10 13:04:19 +0000

    pylint service exception

--- project/src/exception/service_exception.py
+++ project/src/exception/service_exception.py
@@ -1,5 +1,17 @@
+"""
+Module containing a custom exception class for service-related errors.
+"""
+
+
 class ServiceException(Exception):
-    """Custom exception class for errors occuring in the service layer"""
+    """
+    This exception is raised to indicate errors that occur in the service layer
+    of the application. It allows attaching a custom error message to provide more
+    context about the error.
+
+    Args:
+        message (str): The error message describing the reason for the exception.
+    """
 
     def __init__(self, message):
         self.message = message
