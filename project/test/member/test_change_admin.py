3141f506184f134dab4b9b069ee92d8f425f0b29
tomasz-lipka <36009966+tomasz-lipka@users.noreply.github.com>
2023-09-08 09:59:04 +0000

    add token auth

--- project/test/member/test_change_admin.py
+++ project/test/member/test_change_admin.py
@@ -27,16 +27,18 @@ class TestChangeAdmin(TestCase):
 
     def test_normal_run(self):
         register(self.client, 'member')
-        create_space_as_admin(self.client, 'space-1')
-        response = add_member(self.client, 1, 1)
+        token = create_space_as_admin(self.client, 'space-1')
+        response = add_member(self.client, 1, 1, token)
         data = {
             "is-admin": True
         }
-        response = self.client.put('/spaces/1/members/1', json=data)
+        response = self.client.put(
+            '/spaces/1/members/1', json=data, headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b"Admin permission changed")
 
-        response = self.client.get('/spaces/1/members')
+        response = self.client.get(
+            '/spaces/1/members', headers={"Authorization": f"Bearer {token}"})
         expected_data = [
             {
                 "is_admin": True,
@@ -58,75 +60,82 @@ class TestChangeAdmin(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_space_not_exist(self):
-        register_and_login(self.client, 'admin')
+        token = register_and_login(self.client, 'admin')
         data = {
             "is-admin": True
         }
-        response = self.client.put('/spaces/999/members/1', json=data)
+        response = self.client.put(
+            '/spaces/999/members/1', json=data, headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.data, b"Space with ID '999' doesn't exist")
 
     def test_member_not_exist(self):
-        create_space_as_admin(self.client, 'space-1')
+        token = create_space_as_admin(self.client, 'space-1')
         data = {
             "is-admin": True
         }
-        response = self.client.put('/spaces/1/members/999', json=data)
+        response = self.client.put(
+            '/spaces/1/members/999', json=data, headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.data, b"User with ID '999' doesn't exist")
 
     def test_member_from_other_space(self):
         register(self.client, 'member')
-        create_space_as_admin(self.client, 'space-1')
-        add_member(self.client, 1, 1)
+        token = create_space_as_admin(self.client, 'space-1')
+        add_member(self.client, 1, 1, token)
         logout(self.client)
-        create_space_as_admin(self.client, 'space-2')
+        token = create_space_as_admin(self.client, 'space-2')
 
         data = {
             "is-admin": True
         }
-        response = self.client.put('/spaces/2/members/1', json=data)
+        response = self.client.put(
+            '/spaces/2/members/1', json=data, headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 403)
         self.assertEqual(response.data, b'User-space pair doesn\'t exist')
 
     def test_not_admin(self):
-        create_space_as_member(self.client, 'space-1')
+        token = create_space_as_member(self.client, 'space-1')
         data = {
             "is-admin": False
         }
-        response = self.client.put('/spaces/1/members/2', json=data)
+        response = self.client.put(
+            '/spaces/1/members/2', json=data, headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 403)
         self.assertEqual(response.data, b'User not admin')
 
     def test_payload_invalid_type(self):
         register(self.client, 'member')
-        create_space_as_admin(self.client, 'space-1')
-        response = add_member(self.client, 1, 1)
+        token = create_space_as_admin(self.client, 'space-1')
+        response = add_member(self.client, 1, 1, token)
         data = {
             "is-admin": 123
         }
-        response = self.client.put('/spaces/1/members/1', json=data)
+        response = self.client.put(
+            '/spaces/1/members/1', json=data, headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, b'"is admin" must be type boolean')
 
     def test_wrong_json_key(self):
         register(self.client, 'member')
-        create_space_as_admin(self.client, 'space-1')
-        response = add_member(self.client, 1, 1)
+        token = create_space_as_admin(self.client, 'space-1')
+        response = add_member(self.client, 1, 1, token)
         data = {
             "wrong": True
         }
-        response = self.client.put('/spaces/1/members/1', json=data)
+        response = self.client.put(
+            '/spaces/1/members/1', json=data, headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, b"Invalid payload: 'is-admin'")
 
     def test_null_json_value(self):
         register(self.client, 'member')
-        create_space_as_admin(self.client, 'space-1')
-        response = add_member(self.client, 1, 1)
+        token = create_space_as_admin(self.client, 'space-1')
+        response = add_member(self.client, 1, 1, token)
         data = {
             "is-admin": None
         }
-        response = self.client.put('/spaces/1/members/1', json=data)
+        response = self.client.put(
+            '/spaces/1/members/1', json=data, headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, b"Is admin must be provided")
