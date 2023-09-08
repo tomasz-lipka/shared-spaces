46eca955298eb3d95a2ca2b2f4e6cf1eae9fff7b
tomasz-lipka <36009966+tomasz-lipka@users.noreply.github.com>
2023-09-08 10:45:30 +0000

    token auth

--- project/test/space/test_create_space.py
+++ project/test/space/test_create_space.py
@@ -17,55 +17,64 @@ class TestCreateSpace(TestCase):
         purge_db(self.app)
 
     def test_not_logged_in(self):
-        response = create_space(self.client, "space-1")
-        self.assertEqual(response.status_code, 401)
+        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY5NDE2NDIwMSwianRpIjoiNjc0NmNhZGEtNzFjYS00ZGZhLWFkYTUtOTFhYTRlODg2YzZmIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6InRvbSIsIm5iZiI6MTY5NDE2NDIwMSwiZXhwIjoxNjk0MTY1MTAxfQ.GPN8b1ahikw28Iy8cv3zr3gv_MqHfxZktU5zWEiFGT8"
+        response = create_space(self.client, "space-1", token)
+        self.assertEqual(
+            response.data, b'{"msg":"Signature verification failed"}\n')
+        self.assertEqual(response.status_code, 422)
 
     def test_normal_run(self):
-        response = create_space_as_admin(self.client, 'space-1')
+        token = register_and_login(self.client, 'admin')
+        response = create_space(self.client, 'space-1', token)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b"Space created")
 
     def test_wrong_json_key(self):
-        register_and_login(self.client, 'usr')
+        token = register_and_login(self.client, 'usr')
         data = {
             "wrong": "space"
         }
-        response = self.client.post('/spaces', json=data)
+        response = self.client.post(
+            '/spaces', json=data, headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, b"Invalid payload: 'name'")
 
     def test_null_json_value(self):
-        register_and_login(self.client, 'usr')
+        token = register_and_login(self.client, 'usr')
         data = {
             "name": None
         }
-        response = self.client.post('/spaces', json=data)
+        response = self.client.post(
+            '/spaces', json=data, headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, b"Name must be provided")
 
     def test_empty_name(self):
-        register_and_login(self.client, 'usr')
+        token = register_and_login(self.client, 'usr')
         data = {
             "name": "   "
         }
-        response = self.client.post('/spaces', json=data)
+        response = self.client.post(
+            '/spaces', json=data, headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, b"Name cannot be empty")
 
     def test_min_char_name(self):
-        register_and_login(self.client, 'usr')
+        token = register_and_login(self.client, 'usr')
         data = {
             "name": "a"
         }
-        response = self.client.post('/spaces', json=data)
+        response = self.client.post(
+            '/spaces', json=data, headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, b"Name min 3 characters")
 
     def test_max_char_name(self):
-        register_and_login(self.client, 'usr')
+        token = register_and_login(self.client, 'usr')
         data = {
             "name": "name name name name name"
         }
-        response = self.client.post('/spaces', json=data)
+        response = self.client.post(
+            '/spaces', json=data, headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, b"Name max 15 characters")
